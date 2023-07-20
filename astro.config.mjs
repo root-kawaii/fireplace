6f178e00d5125fccae1341499d52a7e81231cc6e
root-kawaii <teo.regge.99@gmail.com>
2023-07-20 16:59:01 +0200

    weblytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://astro-paper.pages.dev/",
   adapter: vercel({
     analytics: true,
