babe6e3be89b5437698f870fff5a7def57bc7c7e
root-kawaii <teo.regge.99@gmail.com>
2023-07-20 17:10:44 +0200

    weblytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/edge";
 
 import vercel from "@astrojs/vercel/serverless";
 
@@ -13,24 +12,34 @@ export default defineConfig({
   output: "server",
   site: "https://astro-paper.pages.dev/",
   adapter: vercel(),
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), sitemap()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, {
-      test: "Table of contents"
-    }]],
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true
+      wrap: true,
     },
-    extendDefaultPlugins: true
+    extendDefaultPlugins: true,
   },
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
